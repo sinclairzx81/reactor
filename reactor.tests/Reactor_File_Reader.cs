bb42cd7f266dca033d9f14eb3569722a0f7fdd8d
sinclairzx81 <haydn.developer@gmail.com>
2015-05-09 23:28:07 +1200

    file reader tests (piped)

--- reactor.tests/Reactor_File_Reader.cs
+++ reactor.tests/Reactor_File_Reader.cs
@@ -28,17 +28,20 @@ THE SOFTWARE.
 
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using System;
+using System.IO;
 using System.Threading.Tasks;
 
-namespace Reactor.Tests
-{
+namespace Reactor.Tests {
+
     [TestClass]
     public class Reactor_File_Reader {
         private static string sequential_file;
+        private static string sequential_file_piped;
 
         [ClassInitialize]
         public static void Startup(TestContext context) {
-            sequential_file = context.TestResultsDirectory + "/sequential.dat";
+            sequential_file       = context.TestResultsDirectory + "/sequential.dat";
+            sequential_file_piped = context.TestResultsDirectory + "/sequential_piped.dat";
             Reactor.Tests.Utility.Files.CreateNumbericSequenceFile(sequential_file, 65536 * 16);
             Reactor.Loop.Start();
         }
@@ -52,7 +55,7 @@ namespace Reactor.Tests
         [TestMethod]
         [TestCategory("Reactor.File.Reader")]
         public async Task Reader_Read_All_Compare_Length () {
-            await Reactor.Async.Future.Create((resolve, reject) => {
+            var future = Reactor.Async.Future.Create((resolve, reject) => {
                 var received = 0;
                 var reader   = Reactor.File.Reader.Create(sequential_file);
                 reader.OnRead(buffer => {
@@ -67,12 +70,14 @@ namespace Reactor.Tests
                     resolve();
                 });
             });
+            Reactor.Timeout.Create(future.Cancel, 1000);
+            await future;
         }
 
         [TestMethod]
         [TestCategory("Reactor.File.Reader")]
         public async Task Reader_Read_All_Sequential_Flowing () {
-            await Reactor.Async.Future.Create((resolve, reject) => {
+            var future = Reactor.Async.Future.Create((resolve, reject) => {
                 var index    = -1;
                 var reader   = Reactor.File.Reader.Create(sequential_file);
                 var expected = (reader.Length / 4) - 1;
@@ -101,22 +106,103 @@ namespace Reactor.Tests
                     resolve();
                 });
             });
+            Reactor.Timeout.Create(future.Cancel, 1000);
+            await future;
         }
 
         [TestMethod]
         [TestCategory("Reactor.File.Reader")]
-        public async Task Reader_Read_Partial() {
-            await Reactor.Async.Future.Create((resolve, reject) => {
-                resolve();
+        public async Task Reader_Read_All_Sequential_NonFlowing () {
+            var future = Reactor.Async.Future.Create((resolve, reject) => {
+                var index    = -1;
+                var reader   = Reactor.File.Reader.Create(sequential_file);
+                var expected = (reader.Length / 4) - 1;
+                reader.OnReadable(() => {
+                    var buffer = reader.Read();
+                    while (buffer.Length > 0) {
+                       if (buffer.Length >= 4) {
+                            var temp = buffer.ReadInt32();
+                            if (temp != (index + 1)) {
+                                reject(new System.Exception("read unexpected ordinal."));
+                                return;
+                            }
+                            index = temp;
+                        }
+                        else {
+                            reader.Unshift(buffer);
+                            break;
+                        }
+                    }
+                });
+                reader.OnError(reject);
+                reader.OnEnd(() => {
+                    if (index != expected) {
+                        reject(new System.Exception("expected index " + index + " got " + expected));
+                        return;
+                    }
+                    resolve();
+                });
             });
+            Reactor.Timeout.Create(future.Cancel, 1000);
+            await future;
         }
 
         [TestMethod]
         [TestCategory("Reactor.File.Reader")]
-        public async Task Reader_Read_Events() {
-            await Reactor.Async.Future.Create((resolve, reject) => {
-                resolve();
+        public async Task Reader_Read_Partial_Sequential_Flowing() {
+            var future = Reactor.Async.Future.Create((resolve, reject) => {
+                //-----------------------------
+                // skip 100 ints, take 100..
+                //-----------------------------
+                var reader   = Reactor.File.Reader.Create(sequential_file, 100 * 4, 100 * 4);
+                var expected = 199;
+                var index    = 99;
+                reader.OnRead(buffer => {
+                    while (buffer.Length > 0) {
+                        if (buffer.Length >= 4) {
+                            var temp = buffer.ReadInt32();
+                            if (temp != (index + 1)) {
+                                reject(new System.Exception("read unexpected ordinal."));
+                                return;
+                            }
+                            index = temp;
+                        }
+                        else {
+                            reader.Unshift(buffer);
+                            break;
+                        }
+                    }
+                });
+                reader.OnError(reject);
+                reader.OnEnd(() => {
+                    if (index != expected) {
+                        reject(new System.Exception("expected index " + index + " got " + expected));
+                        return;
+                    }
+                    resolve();
+                });
             });
+            Reactor.Timeout.Create(future.Cancel, 1000);
+            await future;
+        }
+
+        [TestMethod]
+        [TestCategory("Reactor.File.Reader")]
+        public async Task Reader_Pipe_File_Sequential() {
+            var future = Reactor.Async.Future.Create((resolve, reject) => {
+                var writer = Reactor.File.Writer.Create(sequential_file_piped, FileMode.OpenOrCreate);
+                var reader = Reactor.File.Reader.Create(sequential_file);
+                reader.Pipe(writer);
+                writer.OnEnd(() => {
+                    if (!Reactor.Tests.Utility.Files.AreSame(sequential_file_piped, sequential_file)) {
+                        reject(new Exception("files are not the same"));
+                        return;
+                    }
+                    resolve();
+                });
+            });
+            Reactor.Timeout.Create(future.Cancel, 1000);
+            await future;
         }
     }
 }
